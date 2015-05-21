645df2914c52c523ba2f3385f7469f415f7fee56
FBoss <flavio.boss@stud.hslu.ch>
2015-05-21 10:11:45 +0200

    Anpassung an zielerfassungv2
    Kein Draw mehr in der Detect methode

--- Dev/Treiber/Zielerfassung/zielerfassungv2.py
+++ Dev/Treiber/Zielerfassung/zielerfassungv2.py
@@ -30,12 +30,12 @@ class Zielerfassung(IZielerfassung):
         self.canny = cv2.Canny(self.searcharea, self.config.threshold_min, self.config.threshold_max, apertureSize=3)
 
         #Hough Linien erkennung
-        lines = cv2.HoughLines(self.canny,1,np.pi/180,self.config.hough_threshold)
+        self.lines = cv2.HoughLines(self.canny,1,np.pi/180,self.config.hough_threshold)
 
         self.distance = self.searcharea.shape[1]
 
         #Linien Selektierung
-        for rho,theta in lines[0]:
+        for rho,theta in self.lines[0]:
             #1. Linie ca. senkrecht
             if math.fabs(math.cos(theta - math.pi)) > 0.95:
                 a = np.cos(theta)
@@ -53,7 +53,7 @@ class Zielerfassung(IZielerfassung):
                 if abs(dist) < abs(self.distance):
                     self.distance = dist
 
-                cv2.line(self.searcharea,(x1,y1),(x2,y2),(0,0,255),2)
+                #cv2.line(self.searcharea,(x1,y1),(x2,y2),(0,0,255),2)
 
         #print "Distanz:" +  str(distance)
 
@@ -83,6 +83,20 @@ class Zielerfassung(IZielerfassung):
                 (int(self.config.hitpoint_x+self.distance+self.config.approx_rect_w/2), int(self.config.hitpoint_y+self.config.approx_rect_h)),
                 (255,255,255),3)
 
+        for rho,theta in self.lines[0]:
+            #1. Linie ca. senkrecht
+            if math.fabs(math.cos(theta - math.pi)) > 0.95:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a*rho
+                y0 = b*rho
+                x1 = int(x0 + 1000*(-b))
+                y1 = int(y0 + 1000*(a))
+                x2 = int(x0 - 1000*(-b))
+                y2 = int(y0 - 1000*(a))
+
+                cv2.line(self.searcharea,(x1,y1),(x2,y2),(0,0,255),2)
+
 
         output = np.concatenate((self.input, self.canny), axis=0)
 
@@ -101,9 +115,9 @@ class Zielerfassung(IZielerfassung):
         self.config.save_config()
 
 
-zf = Zielerfassung()
-print "Angle: " + str(zf.detect())
+#zf = Zielerfassung()
+#print "Angle: " + str(zf.detect())
 
-cv2.imshow('image',zf.get_image())
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow('image',zf.get_image())
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
